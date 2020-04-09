327e85d490d0d024a75104c27997f5f3c1fa1040
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2020-04-09 13:24:51 -0300

    [FABOSSI] Fade Transition

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,9 @@
         <transition name="slide" type="animation">
           <div class="alert alert-info" v-if="show">This is some slide info</div>
         </transition>
+        <transition name="slide" type="animation" appear>
+          <div class="alert alert-info" v-if="show">This is some slide info</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -22,7 +25,7 @@
 export default {
   data() {
     return {
-      show: false
+      show: true
     };
   }
 };
