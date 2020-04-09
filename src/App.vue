f70fded958fe6390f0be2d18138e320224ff0db2
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2020-04-09 13:08:47 -0300

    [FABOSSI] Fade Transition

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,44 @@
 <template>
-  <div id="app"></div>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <h1>Animations</h1>
+        <hr />
+        <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+        <br />
+        <br />
+        <transition name="fade">
+          <div class="alert alert-info" v-if="show">This is some Info</div>
+        </transition>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: "Welcome to Your Vue.js App"
+      show: false
     };
   }
 };
 </script>
-
+ 
 <style>
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 1s;
+}
+
+.fade-leave {
+}
+
+.fade-leave-active {
+  transition: opacity 1s;
+  opacity: 0;
+}
 </style>
