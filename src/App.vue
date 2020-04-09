da726027f2f8322250f35a524465f3c69b064fa3
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2020-04-09 13:35:52 -0300

    [FABOSSI] Fade Transition

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,21 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Animations</h1>
         <hr />
+        <select v-model="alertAnimation" class="form-control">
+          <option value="fade">Fade</option>
+          <option value="slide">Slide</option>
+        </select>
+        <br />
         <button class="btn btn-primary" @click="show = !show">Show Alert</button>
         <br />
         <br />
-        <transition name="fade">
+        <transition :name="alertAnimation">
           <div class="alert alert-info" v-if="show">This is some Info</div>
         </transition>
-        <transition name="slide" type="animation">
+        <transition :name="alertAnimation" type="animation">
           <div class="alert alert-info" v-if="show">This is some slide info</div>
         </transition>
-        <transition name="slide" type="animation" appear>
+        <transition enter-active-class="animated bouce" leave-active-class="animated shake">
           <div class="alert alert-info" v-if="show">This is some slide info</div>
         </transition>
       </div>
@@ -25,7 +30,8 @@
 export default {
   data() {
     return {
-      show: true
+      show: true,
+      alertAnimation: "fade"
     };
   }
 };
